172b9e8abf7aaac89a5afbaf55c4c3b3e5c2e7d8
Edwin <pmedwin2004@gmail.com>
2026-05-06 11:08:51 -0500

    Cambio de MySQL a SQLite para despliegue en Render

--- config/settings.py
+++ config/settings.py
@@ -73,21 +73,28 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'gestor_publicidad',       
+#        'USER': 'root',                    
+#        'PASSWORD': '',        
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#       'OPTIONS': {
+#            'charset': 'utf8mb4',
+#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#        },
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestor_publicidad',       
-        'USER': 'root',                    
-        'PASSWORD': '',        
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
